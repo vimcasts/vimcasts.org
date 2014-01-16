d78ccd12c02e59a9bcf51c11058a8f9a424ff9f3
Drew Neil <andrew.jr.neil@gmail.com>
2014-01-16 18:37:24 +0000

    Create /episodes-all archive page

--- lib/aggregator.rb
+++ lib/aggregator.rb
@@ -7,6 +7,10 @@ module Middleman
       def aggregate
         (blog(:episodes).articles + blog(:blog).articles).sort_by(&:date).reverse
       end
+      def minute_second(seconds)
+        min, sec = *seconds.divmod(60)
+        [min, sec.to_s.rjust(2, '0')].join(':')
+      end
     end
   end
   ::Middleman::Extensions.register(:aggregator, Aggregator)
