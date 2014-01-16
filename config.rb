32c17d4c7a2e32a175f4ac7f7c1c3f47b96cba67
Drew Neil <andrew.jr.neil@gmail.com>
2014-01-16 18:43:31 +0000

    Serve file /episodes-all.html at path /episodes/all

--- config.rb
+++ config.rb
@@ -58,6 +58,8 @@ page "/episodes.json", layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+proxy "/episodes/all", "/episodes-all.html"
+
 ###
 # Helpers
 ###
