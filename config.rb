595acd22a120db04be09b36ab40fdbc5c84a6abf
Drew Neil <andrew.jr.neil@gmail.com>
2014-02-07 13:07:54 +0000

    Merge zurb-foundation config.rb into existing file

--- config.rb
+++ config.rb
@@ -58,6 +58,35 @@ page "/episodes.json", layout: false
 # Compass
 ###
 
+compass_config do |config|
+  # Require any additional compass plugins here.
+  config.add_import_path "bower_components/foundation/scss"
+
+  # Set this to the root of your project when deployed:
+  config.http_path = "/"
+  config.css_dir = "stylesheets"
+  config.sass_dir = "stylesheets"
+  config.images_dir = "images"
+  config.javascripts_dir = "javascripts"
+
+  # You can select your preferred output style here (can be overridden via the command line):
+  # output_style = :expanded or :nested or :compact or :compressed
+
+  # To enable relative paths to assets via compass helper functions. Uncomment:
+  # relative_assets = true
+
+  # To disable debugging comments that display the original location of your selectors. Uncomment:
+  # line_comments = false
+
+
+  # If you prefer the indented syntax, you might want to regenerate this
+  # project again passing --syntax sass, or you can uncomment this:
+  # preferred_syntax = :sass
+  # and then run:
+  # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+end
+
 # Change Compass configuration
 # compass_config do |config|
 #   config.output_style = :compact
@@ -83,12 +112,6 @@ page "/episodes.json", layout: false
 # Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
-
-def transcript_count
-  Dir[File.join('source', 'transcripts', '*')].count do |file|
-    File.file?(file)
-  end
-end
 
 def extract_number_from_path(glob)
   Dir[glob].map { |path| path.slice(/\d+/) }
@@ -134,6 +157,12 @@ activate :livereload
 #     "Helping"
 #   end
 # end
+
+# Add bower's directory to sprockets asset path
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
 
 set :css_dir, 'stylesheets'
 
