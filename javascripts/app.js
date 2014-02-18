94efaecea906b26d874e4f2a2c74959ff66e6fd5
Drew Neil <andrew.jr.neil@gmail.com>
2014-02-18 21:11:02 +0000

    Site updated to 5893b23

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,1 @@
-$(document).foundation();
+$(document).foundation();$(document).ready(function() {  if (matchMedia(Foundation.media_queries['medium']).matches) {    $('#shownotes').addClass('active');  }});
