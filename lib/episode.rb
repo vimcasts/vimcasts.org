c4953ef90060e4bcc6686567b97b647499f0d249
Drew Neil <andrew.jr.neil@gmail.com>
2014-02-25 12:36:58 +0000

    Create a VideoFile object to present ogg/quicktime metadata

--- lib/episode.rb
+++ lib/episode.rb
@@ -1,14 +1,47 @@
+require 'json'
+
+class VideoFile
+
+  attr_reader :url, :size, :duration
+
+  def initialize(key, data, metadata_registry='')
+    @registry = metadata_registry
+    if metadata = data[key.to_s]
+      @url = metadata["url"]
+      @size = metadata["size"]
+      @duration = data["duration"]
+    elsif index = data[:number]
+      metadata = load_video_metadata[index.to_s]
+      @url = metadata[key.to_s]["url"]
+      @size = metadata[key.to_s]["size"]
+      @duration = metadata["duration"]
+    end
+  end
+
+private
+
+  def load_video_metadata
+    return unless File.exist?(@registry)
+    File.open(@registry, 'r') do |f|
+      JSON.load(f)
+    end
+  end
+
+end
+
 class Episode
 
-  attr_accessor :poster, :number
+  attr_accessor :poster, :number, :ogg, :quicktime
 
-  def initialize(options={})
+  def initialize(options={}, metadata_registry='data/videos.json')
     if options.respond_to?(:data)
       options = options.data
     end
     @poster = options[:poster]
     @duration = options[:duration].to_i
-    @number = options.fetch(:number, -1).to_s
+    @number = (options[:number] || -1).to_s
+    @ogg = VideoFile.new(:ogg, options, metadata_registry)
+    @quicktime = VideoFile.new(:quicktime, options, metadata_registry)
   end
 
   def poster_url
--- spec/episode_spec.rb
+++ spec/episode_spec.rb
@@ -3,6 +3,10 @@ require 'minitest/autorun'
 require_relative '../lib/episode'
 require 'ostruct'
 
+def registry
+  'spec/video_metadata.json'
+end
+
 describe Episode do
 
   describe 'constructor' do
@@ -35,32 +39,72 @@ describe Episode do
   end
 
   describe 'running_time' do
-
     it 'converts seconds into mm:ss' do
       assert_equal  '0:55', Episode.new({duration:  '55'}).running_time
       assert_equal  '1:01', Episode.new({duration:  '61'}).running_time
       assert_equal '10:11', Episode.new({duration: '611'}).running_time
     end
-
   end
 
   describe 'running_datetime' do
-
     it 'converts seconds into mm:ss' do
       assert_equal  'P0M,55S', Episode.new({duration:  '55'}).running_datetime
       assert_equal  'P1M,01S', Episode.new({duration:  '61'}).running_datetime
     end
-
   end
 
   describe 'number' do
-
     it 'returns -1 when episode has no number' do
       assert_equal '-1', Episode.new().number
     end
     it 'returns given number' do
-      assert_equal '12', Episode.new({number: 12}).number
-      assert_equal '42', Episode.new(OpenStruct.new(data: {number: 42})).number
+      assert_equal '12', Episode.new({number: 12}, registry).number
+      assert_equal '12', Episode.new(OpenStruct.new(data: {number: 12}), registry).number
+    end
+  end
+
+  describe 'ogg' do
+    it 'maps to a VideoFile object' do
+      episode = Episode.new(ogg: {url: 'http://media.vimcasts.org/videos/1/show_invisibles.ogv'})
+      assert_equal VideoFile, episode.ogg.class
+    end
+
+    it 'looks up video metadata indexed by number' do
+      episode = Episode.new({number: 12}, registry)
+      assert_equal 'http://othermedia.vimcasts.org/videos/12/modal_editing.ogv', episode.ogg.url
+      assert_equal 1234567, episode.ogg.size
+    end
+  end
+
+  describe 'quicktime' do
+    it 'maps to a VideoFile object' do
+      episode = Episode.new(quicktime: {url: 'http://media.vimcasts.org/videos/1/show_invisibles.ogv'})
+      assert_equal VideoFile, episode.quicktime.class
+    end
+  end
+
+end
+
+describe VideoFile do
+  describe 'constructor' do
+    it 'can receive url and size directly' do
+      ogg = VideoFile.new(:ogg, {
+        "duration" => 123,
+        "ogg" => {
+          "url" => "http://media.vimcasts.org/videos/12/modal_editing.ogv",
+          "size" => 5036160
+        }
+      })
+      assert_equal 'http://media.vimcasts.org/videos/12/modal_editing.ogv', ogg.url
+      assert_equal 5036160, ogg.size
+      assert_equal 123, ogg.duration
+    end
+
+    it 'can derive url and size by looking up metadata registry' do
+      ogg = VideoFile.new(:ogg, {:number => 12}, 'spec/video_metadata.json')
+      assert_equal 'http://othermedia.vimcasts.org/videos/12/modal_editing.ogv', ogg.url
+      assert_equal 1234567, ogg.size
+      assert_equal 326, ogg.duration
     end
   end
 end
