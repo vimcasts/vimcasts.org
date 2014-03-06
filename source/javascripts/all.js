e5e7cd941aac56f926b597d3159a64cad65cf3ea
Drew Neil <andrew.jr.neil@gmail.com>
2014-03-06 16:33:38 +0000

    Use swiftype jQuery plugins for search forms
    Instead of using #st-search-input id, we're using the class:
    .swiftype-search-input, which means more than one search field can be
    present on the page at once. That also means we can remove the ugly
    activateVisibleSearchField() hack.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,7 @@
 //= require jquery/jquery
+//= require jquery.swiftype.autocomplete
+//= require jquery.ba-hashchange.min
+//= require jquery.swiftype.search
 //= require foundation/js/foundation.min
 
 //= require app
