13b885cab77c0d69d808ab9e7440eae61307dbfb
Drew Neil <andrew.jr.neil@gmail.com>
2014-03-07 08:55:31 +0000

    Use unix fileformat for app.js
    For some reason, the app.js file was using mac fileformat, which caused
    strange results when running `git diff`. Diagnosed and fixed by running
    the following commands in Vim:
    
        :set fileformat?
        mac
        :set fileformat=unix
        :write

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,1 +1,25 @@
-$(document).foundation();$(document).ready(function() {  if (matchMedia(Foundation.media_queries['medium']).matches) {    $('#shownotes').addClass('active');  }  // set up autocomplete  $(".swiftype-search-input").swiftype({    engineKey: "Wdx4LxrBJv86q28yDA8A"  });  // set up search  $(".swiftype-search-input").swiftypeSearch({    engineKey: "Wdx4LxrBJv86q28yDA8A",    resultContainingElement: "#st-results-container",    renderFunction: swiftypeRenderFunction  });  $(".swiftype-search-form").submit(function(event) {    event.preventDefault();    var query = $(".swiftype-search-input", this).val();    window.location = "/results/index.html#stq=" + query;  });});
+$(document).foundation();
+
+$(document).ready(function() {
+  if (matchMedia(Foundation.media_queries['medium']).matches) {
+    $('#shownotes').addClass('active');
+  }
+
+  // set up autocomplete
+  $(".swiftype-search-input").swiftype({
+    engineKey: "Wdx4LxrBJv86q28yDA8A"
+  });
+
+  // set up search
+  $(".swiftype-search-input").swiftypeSearch({
+    engineKey: "Wdx4LxrBJv86q28yDA8A",
+    resultContainingElement: "#st-results-container",
+    renderFunction: swiftypeRenderFunction
+  });
+
+  $(".swiftype-search-form").submit(function(event) {
+    event.preventDefault();
+    var query = $(".swiftype-search-input", this).val();
+    window.location = "/results#stq=" + query;
+  });
+});
