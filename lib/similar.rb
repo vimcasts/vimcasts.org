d3635b2824040cb59c81f8f7429bf94b6853a244
Drew Neil <andrew.jr.neil@gmail.com>
2014-03-27 16:38:27 +0000

    Pad the "Related content" suggestions with recent posts
    This only applies in cases where there are too few similar items.

--- lib/similar.rb
+++ lib/similar.rb
@@ -4,15 +4,26 @@ module Similar
   class Suggester < Struct.new(:app)
 
     # Given an article
-    # Return two similar articles (with >0 common tags):
-    # the predecessor and successor(s) as sorted by date
+    # Return a list of "Related content", drawn from
+    # list of similar (by tag) and recent (by date).
+    # The list should not include the original article
+    # Crop the list to contain the specified count
     def get(article, count=2)
+      items = (related(article, count) + recent).uniq
+      (items - [article]).first(count)
+    end
+
+    # Given an article
+    # Return similar articles (with 1+ common tags):
+    # the predecessor and successor(s) as sorted by date
+    # Note: the list will include the original article
+    def related(article, count)
       candidates = similar(article)
       return [] if candidates.empty?
       target = candidates.index(article)
       indexes(count).map do |index|
         candidates[(target+index) % candidates.length]
-      end.uniq - [article]
+      end
     end
 
     # Given an article (from :blog or :episodes)
@@ -21,12 +32,23 @@ module Similar
     # the specified article, sorted by date
     # Note: the list will include the original article
     def similar(article)
-      @sorted = article.tags.map do |tag|
+      article.tags.map do |tag|
         [
           app.blog(:episodes).tags.fetch(tag, []),
           app.blog(:blog).tags.fetch(tag, [])
         ]
       end.flatten.uniq.sort_by { |e|
+        e.data.date
+      }
+    end
+
+    # Return a list of recent articles/episodes
+    # Note: the list may include the original article
+    def recent
+      [
+        app.blog(:episodes).articles.first(2),
+        app.blog(:blog).articles.first(2)
+      ].flatten.sort_by { |e|
         e.data.date
       }
     end
