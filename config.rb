7023341116c3c12694ffe178b6b4c54e77b8e923
Drew Neil <andrew.jr.neil@gmail.com>
2014-03-27 18:23:18 +0000

    Remove some documentation comments from config.rb

--- config.rb
+++ config.rb
@@ -117,23 +117,6 @@ end
 # Page options, layouts, aliases and proxies
 ###
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
 def extract_number_from_path(glob)
   Dir[glob].map { |path| path.slice(/\d+/) }
 end
