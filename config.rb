965f6604bd38bf5d2469822264b5526eda5b46ab
Drew Neil <andrew.jr.neil@gmail.com>
2014-04-01 17:47:11 +0100

    Remove /episodes/:number/transcript paths
    Prefer now to use /transcripts/:number/{en,fr,de}

--- config.rb
+++ config.rb
@@ -144,23 +144,6 @@ end
 # Page options, layouts, aliases and proxies
 ###
 
-def extract_number_from_path(glob)
-  Dir[glob].map { |path| path.slice(/\d+/) }
-end
-
-glob = File.join("source", "transcripts", "*", "en.md")
-extract_number_from_path(glob).each do |number|
-  proxy "/episodes/#{number}/transcript.html", "/transcripts/#{number}/en.html"
-end
-
-languages = %w{fr}
-languages.each do |lang|
-  glob = File.join("source", "transcripts", "*", "#{lang}.md")
-  extract_number_from_path(glob).each do |number|
-    proxy "/episodes/#{number}/transcript/#{lang}.html", "/transcripts/#{number}/#{lang}.html"
-  end
-end
-
 proxy "/episodes/archive.html", "/episodes-all.html", ignore: true
 
 # Generate data/categories.yml with `rake dump_categories`
